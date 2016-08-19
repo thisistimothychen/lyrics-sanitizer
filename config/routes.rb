718d64c1bc2c4ea68876cd86539a78dd883b787a
Timothy Chen <timothy.chen01@gmail.com>
2016-08-19 00:09:46 -0400

    Remove controller from URL

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
-  get 'pages/home'
-  get 'pages/analysis'
-  post 'pages/analysis'
+  get 'home' => 'pages#home'
+  get 'analysis' => 'pages#analysis'
+  post 'analysis' => 'pages#analysis'
   root 'pages#home'
 end
